07968c0e3324ddff6e98c918d74cdec338cc0f71
Clement xen <140471915+ClementMabila@users.noreply.github.com>
2024-08-26 05:08:17 +0200

    Update settings.py

--- ContractsApplication/settings.py
+++ ContractsApplication/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'ContractsApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CraniumInternsDataBase',
-        'USER': 'postgres',
-        'PASSWORD': 'zeneth',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
